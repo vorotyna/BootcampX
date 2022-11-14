bcb69c8af56150e3199b823cd55777cb7b8aec44
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-11-14 23:30:40 +0000

    Update

--- 3_queries_group-by/6_low_average_completion_time.sql
+++ 3_queries_group-by/6_low_average_completion_time.sql
@@ -1,7 +1,8 @@
 SELECT students.name AS student, AVG(assignment_submissions.duration) AS average_assignment_duration, AVG(assignments.duration) AS average_estimated_duration
 FROM assignment_submissions
-JOIN assignments ON assignment_id = assignments.id
 JOIN students ON student_id = students.id
+JOIN assignments ON assignment_id = assignments.id
 WHERE end_date IS NULL
 GROUP BY student
+HAVING AVG(assignment_submissions.duration) < AVG(assignments.duration)
 ORDER BY average_assignment_duration DESC;
