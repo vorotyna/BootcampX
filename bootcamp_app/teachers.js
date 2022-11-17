f4a15bd23018d2bc8a956db56bd75d7834b6dede
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-11-17 02:59:48 +0000

    Protect against malicious SQL injections

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,15 +9,20 @@ const pool = new Pool({
 });
 
 // QUERYING THE DATABASE
-pool.query(`
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
+
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${process.argv[2]}'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+  `;
+
+pool.query(queryString, values)
   .then(res => {
     console.log('connected');
     res.rows.forEach(user => {
@@ -25,6 +30,3 @@ ORDER BY teacher;
     });
   })
   .catch(err => console.error('query error', err.stack));
-
-
-
